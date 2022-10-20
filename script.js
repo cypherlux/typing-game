bf7ca3bb43219494a39798a9af5b61d0c4c046d9
cypherlux <89822852+cypherlux@users.noreply.github.com>
2022-10-19 17:46:23 -0700

    updated styling and added a score tracker

--- script.js
+++ script.js
@@ -11,14 +11,25 @@ const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start');
 const message = document.getElementById('message');
+const gamerName = document.getElementById("gamer-name");
+const scores = getScores();
+const scoresUnorderedList = document.getElementById("scores-unordered-list");
 
 let wordQueue;
 let highlightPosition;
 let startTime;
 
 
-function startGame(){
+function startGame() {
     console.log("Game started");
+    const scoreItem = {
+        name: gamerName.value,
+        milliseconds: 0
+    };
+
+    scores.push(scoreItem);
+
+
     document.body.className = "";
     start.className = "started";
 
@@ -33,6 +44,10 @@ function startGame(){
     quote.childNodes[highlightPosition].className = 'highlight';
 
     startTime = new Date().getTime();
+    
+    document.body.className = "";
+    start.className = "started";
+    setTimeout(() => {start.className = "button";}, 2000);
 }
 start.addEventListener('click', startGame);
 input.addEventListener('input', checkInput);
@@ -59,11 +74,46 @@ function checkInput() {
     quote.childNodes[highlightPosition].className = 'highlight';
 }
 
-function gameOver(){
+function gameOver() {
     const elapsedTime = new Date().getTime() - startTime;
     document.body.className = "winner";
     message.innerHTML = `<span class="congrats">Congratulations!</span>
     <br>
     You finished in ${elapsedTime/1000} seconds`;
 
+    const lastScoreItem = scores.pop();
+    lastScoreItem.milliseconds = elapsedTime;
+    scores.push(lastScoreItem);
+    console.log('The scores array at the end of the game is ', scores);
+    saveScores();
+
+    //clear out old list in DOM
+    while(scoresUnorderedList.firstChild) {
+        scoresUnorderedList.removeChild(scoresUnorderedList.firstChild);
+    }
+
+    for(let score of getScores()) {
+        const li = createElementForScore(score);
+        scoresUnorderedList.appendChild(li);
+    }
+  }
+
+function getScores() {
+const noScoresFound = '[]';
+const scoresJSON = localStorage.getItem('scores') || noScoresFound;
+return JSON.parse(scoresJSON);
+}
+
+function saveScores() {
+const data = JSON.stringify(scores);
+localStorage.setItem('scores', data);
+}
+
+function createElementForScore(score){
+    const template = document.getElementById("score-item-template");
+    const newListItem = template.content.cloneNode(true);
+
+    const text = newListItem.querySelector(".score-text");
+    text.innerHTML = score.name + " in " + score.milliseconds/1000 + " seconds.";
+    return newListItem;
 }
